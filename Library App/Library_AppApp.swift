72df1d3d2c42da5119677659b3345aa804feae53
cmsho <55258597+cmsho@users.noreply.github.com>
2021-09-07 19:31:54 -0400

    Initial Commit

---
+++ Library App/Library_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Library_AppApp.swift
+//  Library App
+//
+//  Created by Colton Shoenberger on 9/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Library_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
